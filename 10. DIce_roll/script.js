b96c37658f6bd181946bde48f8d444f1401cc8c3
Ancil-Benny <ancilbennyka@gmail.com>
2025-02-22 19:19:22 +0530

    add different approach

--- 10. DIce_roll/script.js
+++ 10. DIce_roll/script.js
@@ -1,48 +1,15 @@
-const input = document.getElementById('diceinp');
-const submit = document.getElementById('submit');
+function rollDice(){
+const input = document.getElementById('diceinp').value;
+const diceimg = document.getElementById('diceimg');
 const label = document.getElementById('dicenum');
-const d1 = document.getElementById('d1');
-const d2 = document.getElementById('d2');
-const d3 = document.getElementById('d3');
-const d4 = document.getElementById('d4');
-const d5 = document.getElementById('d5');
-const d6 = document.getElementById('d6');
+const values = [];
+const images = [];
 
-const nums = [];
-submit.onclick = function(){
-    d1.style.display = "none";
-    d2.style.display = "none";
-    d3.style.display = "none";
-    d4.style.display = "none";
-    d5.style.display = "none";
-    d6.style.display = "none";
-nums.length = 0;
-const number = input.value;
-if(isNaN(number) || number>6 || number<1){
-    alert('invalid');
+for(let i = 0;i<input;i++){
+    const value = Math.floor(Math.random()*6)+1;
+    values.push(value);
+    images.push(`<img src="Dices/d${value}.png"`)
 }
-else{
-getRandom(number);
-for(let n of nums){
- if(n==1)
-    d1.style.display = "block";
-else if(n==2)
-    d2.style.display = "block";
-else if(n==3)
-    d3.style.display = "block";
-else if(n==4)
-    d4.style.display = "block";
-else if(n==5)
-    d5.style.display = "block";
-else if(n==6)
-    d6.style.display = "block";
-}
-}
-}
+label.textContent =  `dice: ${values.join(', ')}`;
 
-function getRandom(n){
-    for(let i = 0; i<n;i++){
-    let rand = Math.floor(Math.random()*6)+1;
-    nums.push(rand);
-    }
 }
