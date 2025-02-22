2f7f9ff94b8fa9b6441826e2750460ed58a84e96
Ancil-Benny <ancilbennyka@gmail.com>
2025-02-22 19:28:31 +0530

    add new styles and complete the scripts

--- 10. DIce_roll/script.js
+++ 10. DIce_roll/script.js
@@ -1,15 +1,16 @@
-function rollDice(){
-const input = document.getElementById('diceinp').value;
-const diceimg = document.getElementById('diceimg');
-const label = document.getElementById('dicenum');
-const values = [];
-const images = [];
+function rollDice() {
+    const input = document.getElementById('diceinp').value;
+    const diceimg = document.getElementById('diceimg');
+    const label = document.getElementById('dicenum');
+    const values = [];
+    const images = [];
 
-for(let i = 0;i<input;i++){
-    const value = Math.floor(Math.random()*6)+1;
-    values.push(value);
-    images.push(`<img src="Dices/d${value}.png"`)
-}
-label.textContent =  `dice: ${values.join(', ')}`;
+    for (let i = 0; i < input; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="Dices/d${value}.png" alt="Dice ${value}" style="margin: 5px">`);
+    }
 
+    label.textContent = `dice: ${values.join(', ')}`;
+    diceimg.innerHTML = images.join('');
 }
